7813207f39f59998d33006320605da4be88f920c
rydersir <jmoresca@outlook.com>
2024-07-13 17:29:13 +0200

    refactor drawer and modal components

--- actionFlow.editor/src/components/Flow.tsx
+++ actionFlow.editor/src/components/Flow.tsx
@@ -16,6 +16,8 @@ import "@xyflow/react/dist/style.css";
 import { initialNodes, nodeTypes, type CustomNodeType } from "./nodes";
 import { initialEdges, edgeTypes, type CustomEdgeType } from "./edges";
 import { Button, Drawer, Modal } from "flowbite-react";
+import ActionDrawer from "./left-pane/ActionDrawer";
+import AddActionModal from "./left-pane/AddActionModal";
 
 export default function App() {
   const [nodes, , onNodesChange] = useNodesState<CustomNodeType>(initialNodes);
@@ -26,11 +28,15 @@ export default function App() {
     [setEdges]
   );
 
-  const [openModal, setOpenModal] = useState(false);
+  const [showAddActionModal, setOpenAddActionModal] = useState(false);
 
-  const actionIcon = (<svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-    <path fillRule="evenodd" d="M3 4a1 1 0 0 0-.822 1.57L6.632 12l-4.454 6.43A1 1 0 0 0 3 20h13.153a1 1 0 0 0 .822-.43l4.847-7a1 1 0 0 0 0-1.14l-4.847-7a1 1 0 0 0-.822-.43H3Z" clipRule="evenodd" />
-  </svg>)
+  const handleAddAction = () => {
+    setOpenAddActionModal(true);
+  }
+
+  const handleCloseAddActionModal = () => {
+    setOpenAddActionModal(false);
+  }
 
   return (
     <ReactFlow<CustomNodeType, CustomEdgeType>
@@ -51,96 +57,12 @@ export default function App() {
       <MiniMap />
 
       <Panel position="top-left">
-
-        <Drawer open={true} onClose={() => { }} backdrop={false}>
-          <Drawer.Header title="Drawer" />
-          <Drawer.Items>
-            <p className="mb-6 text-sm text-gray-500 dark:text-gray-400">
-              Supercharge your hiring by taking advantage of our&nbsp;
-              <a href="#" className="text-cyan-600 underline hover:no-underline dark:text-cyan-500">
-                limited-time sale
-              </a>
-              &nbsp;for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the #1 design
-              job board.
-            </p>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-              <a
-                href="#"
-                className="rounded-lg border border-gray-200 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-cyan-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
-              >
-                Learn more
-              </a>
-              <a
-                href="#"
-                className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-              >
-                Get access&nbsp;
-                <svg
-                  className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </a>
-            </div>
-
-            <Button.Group>
-              <Button color="gray" onClick={() => setOpenModal(true)}>Add</Button>
-              <Button color="gray">Delete</Button>
-            </Button.Group>
-
-          </Drawer.Items>
-        </Drawer>
+        <ActionDrawer addAction={handleAddAction} />
       </Panel>
 
-      <Controls />
+      <Controls className="left-80" />
 
-      <Modal show={openModal} size="xl" onClose={() => setOpenModal(false)} popup>
-        <Modal.Header />
-        <Modal.Body>
-          <div className="grid grid-cols-3 gap-4 p-4 lg:grid-cols-4">
-            <div className="cursor-pointer rounded-lg bg-gray-50 p-4 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600">
-              <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-gray-200 p-2 dark:bg-gray-600">
-                {actionIcon}
-              </div>
-              <div className="text-center font-medium text-gray-500 dark:text-gray-400">Set Variable</div>
-            </div>
-            <div className="cursor-pointer rounded-lg bg-gray-50 p-4 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600">
-              <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-gray-200 p-2 dark:bg-gray-600">
-                {actionIcon}
-              </div>
-              <div className="text-center font-medium text-gray-500 dark:text-gray-400">Loop</div>
-            </div>
-            <div className="cursor-pointer rounded-lg bg-gray-50 p-4 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600">
-              <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-gray-200 p-2 dark:bg-gray-600">
-                {actionIcon}
-              </div>
-              <div className="text-center font-medium text-gray-500 dark:text-gray-400">Http Call</div>
-            </div>
-            <div className="cursor-pointer rounded-lg bg-gray-50 p-4 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600">
-              <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-gray-200 p-2 dark:bg-gray-600">
-                {actionIcon}
-              </div>
-              <div className="text-center font-medium text-gray-500 dark:text-gray-400">Control Flow</div>
-            </div>
-            <div className="cursor-pointer rounded-lg bg-gray-50 p-4 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600">
-              <div className="mx-auto mb-2 flex h-[48px] max-h-[48px] w-[48px] max-w-[48px] items-center justify-center rounded-full bg-gray-200 p-2 dark:bg-gray-600">
-                {actionIcon}
-              </div>
-              <div className="text-center font-medium text-gray-500 dark:text-gray-400">Call Workflow</div>
-            </div>
-          </div>
-        </Modal.Body>
-      </Modal>
+      <AddActionModal showModal={showAddActionModal} closeModal={handleCloseAddActionModal} />
 
     </ReactFlow>
   );
