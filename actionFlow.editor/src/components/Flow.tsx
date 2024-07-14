7ceaefb2d4b34eb066f8fc17697e110b68c48050
jakemoresca <jmoresca@outlook.com>
2024-07-14 23:51:31 +0200

    add delete and add mock actions

--- actionFlow.editor/src/components/Flow.tsx
+++ actionFlow.editor/src/components/Flow.tsx
@@ -9,18 +9,23 @@ import {
   useEdgesState,
   type OnConnect,
   Panel,
+  Node,
+  Edge,
+  getIncomers,
+  getOutgoers,
+  getConnectedEdges,
 } from "@xyflow/react";
 
 import "@xyflow/react/dist/style.css";
 
 import { initialNodes, nodeTypes, type CustomNodeType } from "./nodes";
 import { initialEdges, edgeTypes, type CustomEdgeType } from "./edges";
-import { Button, Drawer, Modal } from "flowbite-react";
 import ActionDrawer from "./left-pane/ActionDrawer";
 import AddActionModal from "./left-pane/AddActionModal";
 
 export default function App() {
-  const [nodes, , onNodesChange] = useNodesState<CustomNodeType>(initialNodes);
+  const [nodes, setNodes, onNodesChange] =
+    useNodesState<CustomNodeType>(initialNodes);
   const [edges, setEdges, onEdgesChange] =
     useEdgesState<CustomEdgeType>(initialEdges);
   const onConnect: OnConnect = useCallback(
@@ -29,14 +34,53 @@ export default function App() {
   );
 
   const [showAddActionModal, setOpenAddActionModal] = useState(false);
+  const [selectedNodes, setSelectedNodes] = useState<Node[]>([]);
 
   const handleAddAction = () => {
     setOpenAddActionModal(true);
-  }
+  };
 
   const handleCloseAddActionModal = () => {
     setOpenAddActionModal(false);
-  }
+  };
+
+  const handleFlowSelectionChange = (params: {
+    nodes: Node[];
+    edges: Edge[];
+  }) => {
+    setSelectedNodes(params.nodes);
+  };
+
+  const handleDeleteNodes = useCallback(() => {
+    selectedNodes.forEach((selectedNode) => {
+      setEdges((edges) => {
+        const incomers = getIncomers(selectedNode, nodes, edges);
+        const outgoers = getOutgoers(selectedNode, nodes, edges);
+        const connectedEdges = getConnectedEdges([selectedNode], edges);
+
+        const remainingEdges = edges.filter(
+          (edge) => !connectedEdges.includes(edge)
+        );
+
+        const createdEdges = incomers.flatMap(({ id: source }) =>
+          outgoers.map(({ id: target }) => ({
+            id: `${source}->${target}`,
+            source,
+            target,
+          }))
+        );
+
+        return [...remainingEdges, ...createdEdges];
+      });
+    });
+
+    setNodes((nodes) =>
+      nodes.filter(
+        (node) =>
+          selectedNodes.find((selected) => selected.id == node.id) == undefined
+      )
+    );
+  }, [selectedNodes, nodes, edges, setNodes, setEdges]);
 
   return (
     <ReactFlow<CustomNodeType, CustomEdgeType>
@@ -50,6 +94,7 @@ export default function App() {
       nodesConnectable={true}
       nodesDraggable={true}
       elementsSelectable={true}
+      onSelectionChange={handleFlowSelectionChange}
       fitView
       className="bg-white dark:bg-gray-900 antialiased"
     >
@@ -57,13 +102,19 @@ export default function App() {
       <MiniMap />
 
       <Panel position="top-left">
-        <ActionDrawer addAction={handleAddAction} />
+        <ActionDrawer
+          onAddAction={handleAddAction}
+          selectedNodes={selectedNodes}
+          onDeleteAction={handleDeleteNodes}
+        />
       </Panel>
 
       <Controls className="left-80" />
 
-      <AddActionModal showModal={showAddActionModal} closeModal={handleCloseAddActionModal} />
-
+      <AddActionModal
+        showModal={showAddActionModal}
+        onCloseModal={handleCloseAddActionModal}
+      />
     </ReactFlow>
   );
 }
--- actionFlow.editor/src/components/left-pane/ActionDrawer.tsx
+++ actionFlow.editor/src/components/left-pane/ActionDrawer.tsx
@@ -1,57 +1,36 @@
 import { Drawer, Button } from "flowbite-react"
+import { Node } from "@xyflow/react";
 
 export type ActionDrawerData = {
-  addAction?: () => void;
-  deleteAction?: () => void;
+  onAddAction?: () => void;
+  onDeleteAction?: () => void;
+  selectedNodes?: Node[];
 }
 
-export default function ActionDrawer({ addAction, deleteAction }: ActionDrawerData) {
+export default function ActionDrawer({ onAddAction: addAction, onDeleteAction, selectedNodes }: ActionDrawerData) {
+
+    const validNodeTypesToDelete = [
+      "variable"
+    ]
+
+    const validNodeTypesToAddTo = [
+      "input",
+      "variable"
+    ]
+
+    const canDelete = selectedNodes && selectedNodes.length > 0 && selectedNodes?.every(x => {
+      return x.type && validNodeTypesToDelete.includes(x.type);
+    })
+
+    const canAdd = selectedNodes && selectedNodes.length == 1 && selectedNodes[0].type && validNodeTypesToAddTo.includes(selectedNodes[0].type); 
 
     return (
         <Drawer open={true} onClose={() => { }} backdrop={false}>
-          <Drawer.Header title="Drawer" />
+          <Drawer.Header title="Action Flow Editor" />
           <Drawer.Items>
-            <p className="mb-6 text-sm text-gray-500 dark:text-gray-400">
-              Supercharge your hiring by taking advantage of our&nbsp;
-              <a href="#" className="text-cyan-600 underline hover:no-underline dark:text-cyan-500">
-                limited-time sale
-              </a>
-              &nbsp;for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the #1 design
-              job board.
-            </p>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
-              <a
-                href="#"
-                className="rounded-lg border border-gray-200 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-cyan-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
-              >
-                Learn more
-              </a>
-              <a
-                href="#"
-                className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-              >
-                Get access&nbsp;
-                <svg
-                  className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 14 10"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M1 5h12m0 0L9 1m4 4L9 9"
-                  />
-                </svg>
-              </a>
-            </div>
-
             <Button.Group>
-              <Button color="gray" onClick={() => addAction && addAction()}>Add</Button>
-              <Button color="gray">Delete</Button>
+              <Button color="gray" disabled={!canAdd} onClick={() => addAction && addAction()}>Add</Button>
+              <Button color="gray" disabled={!canDelete} onClick={() => onDeleteAction && onDeleteAction()}>Delete</Button>
             </Button.Group>
 
           </Drawer.Items>
